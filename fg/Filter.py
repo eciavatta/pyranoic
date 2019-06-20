5182ff1d80c01d4df5b2f5b99b252a6169400b0b
gio <andrea.giovine@hotmail.it>
2019-06-20 15:09:02 +0200

    recreate stream

--- fg/Filter.py
+++ fg/Filter.py
@@ -3,21 +3,28 @@ from collections import defaultdict
 
 #cap = pyshark.FileCapture('~/traffic/prova.cap')
 
-filtered_cap = pyshark.FileCapture('~/traffic/prova.pcap', display_filter='tcp')
+filtered_cap = pyshark.FileCapture('/home/gio/traffic/prova.pcap', display_filter='tcp')
 
 stream = defaultdict(list)
 
 for packet in filtered_cap:
-    source = packet.ip.src
-    destination = packet.ip.dst
-    connection = source + destination
-    if connection not in stream:
-        stream[connection] = [packet]
+    source_ip = packet.ip.src
+    destination_ip = packet.ip.dst
+
+    source_port = packet.tcp.port
+    destination_port = packet.tcp.dstport
+
+    source = (source_ip, source_port)
+    dest = (destination_ip, destination_port)
+
+    if (dest, source) in stream:
+        stream[(dest, source)].append(packet)
+
     else:
-        stream[connection].append(packet)
+        stream[(source, dest)].append(packet)
 
-print(connection)
 
+print(stream)
 
 
 
